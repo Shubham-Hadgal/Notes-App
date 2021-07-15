e39ce39d2ab09fc35343e8bea24bc5fe3221debd
Shubham Hadgal <shubhamhadgal45@gmail.com>
2021-07-15 21:02:50 +0530

    Stable build

---
+++ app/src/main/java/com/example/notes/NoteRepository.kt
@@ -0,0 +1,18 @@
+package com.example.notes
+
+import androidx.lifecycle.LiveData
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    val allNotes: LiveData<List<Note>> = noteDao.getALlNotes()
+
+    suspend fun insert(note: Note)
+    {
+        noteDao.insert(note)
+    }
+
+    suspend fun delete(note: Note)
+    {
+        noteDao.delete(note)
+    }
+}
